a11ae9e96989de44d3df9b2c0de78cf8121412f1
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-09 04:39:18 -0700

    Betty would be proud

---
+++ greet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * greet - Prints a greeting message
+ *
+ * Description: This function prints "Hello, world!" to the console.
+ */
+void greet(void)
+{
+	print("Hello, world!\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	greet();
+	return (0);
+}
