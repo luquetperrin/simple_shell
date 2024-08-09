8026149445a3b40c1dbeb7b1314875985a93453e
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-09 04:55:51 -0700

    Simple shell 0.1

---
+++ simple_shell_program.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *argv[2];
+    pid_t pid;
+
+    while (1)
+    {
+        printf("#cisfun$ ");
+        read = getline(&line, &len, stdin);
+
+        if (read == -1)
+        {
+            printf("\n");
+            break;
+        }
+
+        line[strcspn(line, "\n")] = 0;
+
+        argv[0] = line;
+        argv[1] = NULL;
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            if (execve(argv[0], argv, NULL) == -1)
+            {
+                perror("./shell");
+            }
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    free(line);
+    return 0;
+}
